986c1dac1cdc270cc9a50ad65b318e8020060667
Asad Sayeed <ashasd97@gmail.com>
2022-10-02 18:01:35 +0530

    Initial commit

---
+++ src/com/asad/Main.java
@@ -0,0 +1,26 @@
+package com.asad;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        /*
+        * Syntax of for loops:
+        * s
+        *
+        *
+        *
+        *
+        *
+        * */
+        for (int num=1; num<=5;num += 1){
+            System.out.println(num);
+        }
+
+    }
+
+
+
+}
