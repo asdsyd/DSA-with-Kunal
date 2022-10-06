eb5cb19a9c189bbc79c9b490feea4363999b06fe
Asad Sayeed <ashasd97@gmail.com>
2022-10-06 19:10:31 +0530

    Arrays lecture

--- src/com/asad/Arrays.java
+++ src/com/asad/Arrays.java
@@ -1,4 +1,27 @@
 package com.asad;
 
 public class Arrays {
+
+    public static void main(String[] args) {
+        //You have to store a roll number....
+        int a = 85;
+
+        // storing a prson name
+        String name = "Asad";
+
+
+//        Store 5 roll nos ?/
+        int [] rnos = new int[5];
+        //or direct way
+        int[] rnos2 = {7,8,45,85,99};
+//        ting ting ting
+
+        //dynamic memory allocation
+        //internally how it works?
+        //arr is in stack
+        //arr will be pointing towards the heap which stores the object
+         
+
+
+    }
 }
