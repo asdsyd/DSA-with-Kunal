7b421c15401679de46099e8228f33ac7486e4b1e
Asad Sayeed <ashasd97@gmail.com>
2022-10-22 23:28:40 +0530

    SelectionSort

--- src/com/asad/InsertionSort.java
+++ src/com/asad/InsertionSort.java
@@ -1,5 +1,43 @@
 package com.asad;
 
+import java.util.ArrayList;
+
 public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 2,-32,0,89,1};
+        selection(arr);
+        int [] ans = selection(arr);
+        System.out.println(ans);
+    }
+
+    static void selection(int [] arr) {
+        for (int i = 0;i <arr.length;i++){
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+
+        }
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+
+        for (int i = start; i <= end; i ++){
+                if (arr[max] < arr[i]){
+                    max = i;
+                }
+        }
+
+        return max;
+    }
+
+    static void swap(int []arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
 
 }
