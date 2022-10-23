f4a707bd2baa2c9db5d85cdd012be788ca55e9ae
Asad Sayeed <ashasd97@gmail.com>
2022-10-23 23:44:24 +0530

    SelectionSort

---
+++ src/com/asad/Pivot.java
@@ -0,0 +1,19 @@
+package com.asad;
+//https://leetcode.com/problems/find-pivot-index/description/
+public class Pivot {
+    class Solution {
+        public int pivotIndex(int[] nums) {
+            if(nums.length == 0) return - 1;
+            int leftSum = 0, rightSum = 0;
+            for(int num : nums)
+                rightSum += num;
+
+            for(int i = 0; i < nums.length; i ++) {
+                rightSum -= nums[i];
+                if(rightSum == leftSum) return i;
+                leftSum += nums[i];
+            }
+            return - 1;
+        }
+    }
+}
